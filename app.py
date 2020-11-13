d42f9760f3027f6ac5890e469969f98da76668f6
datageekrj <55853415+datageekrj@users.noreply.github.com>
2020-11-13 16:05:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,42 @@
-from flask import Flask, render_template,request
-import os
 import speech_recognition as sr 
 import moviepy.editor as mp
+from flask import Flask, request, render_template
+def extract_text(video_path, audio_path, duration = 20):
+    clip = mp.VideoFileClip(video_path)
+    clip.audio.write_audiofile(audio_path)
+    recognizer = sr.Recognizer()
+    audio = sr.AudioFile("audio.wav")
+    with audio as source:
+        audio_file = recognizer.record(source, duration=duration)
+
+    result = recognizer.recognize_google(audio_file)
+    return result
 
 app = Flask(__name__)
+
+import os 
 folder_to_save = os.path.join(app.instance_path, "uploads")
 os.makedirs(folder_to_save, exist_ok=True)
 
-@app.route('/', methods = ["GET", "POST"])
-def hello_world():
+
+@app.route("/", methods = ["GET", "POST"])
+def index():
     if request.method == "POST":
         file = request.files["img"]
         file.save(os.path.join(folder_to_save, file.filename))
-        def_value = 10
+        # We define two paths
+        audio_path = os.path.join(folder_to_save, "toBeCovt.wav")
+        video_path = os.path.join(folder_to_save, file.filename)
+
+        # Get the number of seconds
+        def_value = 20
         number = request.form.get("seconds", def_value)
-        clip = mp.VideoFileClip(os.path.join(folder_to_save, file.filename)) 
-        clip.audio.write_audiofile(os.path.join(folder_to_save, "toBeConvt.wav"))
-        clip.close()
-        r = sr.Recognizer()
-        audio = sr.AudioFile(os.path.join(folder_to_save, "toBeConvt.wav"))
-        with audio as source:
-            audio_file = r.record(source, duration = int(number))
-        result = r.recognize_google(audio_file)
-        os.remove(os.path.join(folder_to_save, "toBeConvt.wav"))
-        os.remove(os.path.join(folder_to_save, file.filename))
+        number = int(number)
+
+        # Use the path with extract_text function
+        result = extract_text(video_path, audio_path, duration=number)
         return render_template("index.html", result = result)
-    else:
-        return render_template("index.html")
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
